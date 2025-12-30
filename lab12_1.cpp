2d0e16efe1a4b3db9a5ff4d2025860ffb64ae9ac
Atom <chanidapa.atom@gmail.com>
2025-12-30 17:38:29 +0700

    lab12_1.cpp improved

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int x){
+long long int fibonacci(long long int x){
     if (x>1){
-        int y = fibonacci(x-1)+ fibonacci(x-2);
+        long long int y = fibonacci(x-1)+ fibonacci(x-2);
         return y;
     } else if (x==1){
         return 1;
@@ -13,10 +14,12 @@ int fibonacci(int x){
 }
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
 int fibonacci(int);
