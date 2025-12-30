643ce450fc5328116d59cbe6b98050733a0d4d0c
Atom <chanidapa.atom@gmail.com>
2025-12-30 17:30:35 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x){
+    if (x>1){
+        int y = fibonacci(x-1)+ fibonacci(x-2);
+        return y;
+    } else if (x==1){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int);
